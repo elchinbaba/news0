2883b5046a2fb7e9c4bdba1c799a4d13b7702948
elchinbaba <babalielchin@gmail.com>
2023-06-08 14:38:49 +0400

    Add NewsService

---
+++ News0.Application/Services/DbOperations/NewsService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace News0.Application.Services.DbOperations
+{
+    public class NewsService
+    {
+        private readonly NewsContextApplication _context;
+        public NewsService(NewsContextApplication context)
+        {
+            _context = context;
+        }
+
+        public void Create(Domain.Entities.PostTranslation news)
+        {
+            _context.PostTranslations.Add(news);
+            _context.SaveChanges();
+        }
+
+        public void Update(Domain.Entities.PostTranslation news)
+        {
+            _context.PostTranslations.Update(news);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Domain.Entities.PostTranslation news)
+        {
+            _context.PostTranslations.Remove(news);
+            _context.SaveChanges();
+        }
+    }
+}
