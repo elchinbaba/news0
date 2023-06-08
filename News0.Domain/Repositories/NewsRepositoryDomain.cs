2c46356bbd9b767284d5187fd3670747825c1017
elchinbaba <babalielchin@gmail.com>
2023-06-08 14:42:11 +0400

    Add NewsRepositoryDomain

---
+++ News0.Domain/Repositories/NewsRepositoryDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace News0.Domain.Repositories
+{
+    public interface NewsRepositoryDomain
+    {
+        public void Select();
+        public void Select(int id);
+        public void Create(Entities.Category category);
+        public void Update(Entities.Category category);
+        public void Delete(Entities.Category category);
+    }
+}
